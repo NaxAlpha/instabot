7577d143f8ec5410205b8aa5e2fea25ba7277389
Nauman Mustafa <nauman.mustafa.x@gmail.com>
2018-09-15 22:48:46 +0500

    v1

--- libinsta.py
+++ libinsta.py
@@ -1,5 +1,6 @@
 # Social Automation Library
 import re
+from logger import *
 from config import *
 from time import sleep
 from functools import wraps
@@ -15,6 +16,15 @@ from selenium.common.exceptions import StaleElementReferenceException, NoSuchEle
 
 user_link_regex = re.compile(USER_LINK_REGEX)
 post_link_regex = re.compile(POST_LINK_REGEX)
+
+
+def logged(fx):
+	@wraps(fx)
+	def _fx(*args, **kwargs):
+		log('enter', fx.__name__)
+		fx(*args, **kwargs)
+		log('exit', fx.__name__)
+	return _fx
 
 
 def wait_for_element(browser, by, value):
@@ -176,17 +186,30 @@ class User:
 			yield get_user_id(item)
 		browser.execute_script('return arguments[0].parentNode;', context).click()
 		random_wait()
-		
+	
 	@property
 	def followers(self):
 		return self._follow('/followers/')
 	
 	@property
-	def followering(self):
+	def followed_by(self):
 		return self._follow('/following/')
 	
 	def posts(self, count=-1):
-		pass
+		for p in search_objects(self.browser, is_post_link, count):
+			yield get_post_id(p)
+	
+	@property
+	def following(self):
+		return self.browser.\
+			find_element_by_css_selector('header section span>span>button').\
+			text == 'Following'
+	
+	@property
+	def toggle_follow(self):
+		return self.browser.\
+			find_element_by_css_selector('header section span>span>button').\
+			click()
 	
 	@staticmethod
 	def default():
@@ -197,20 +220,15 @@ class Instagram:
 	
 	def __init__(self, browser: webdriver.Chrome):
 		self.browser = browser
-	
-	def wait(self):
-		random_wait()
 		
 	def wait_for(self, by, value):
-		self.wait()
-		wait_for_element(self.browser, by, value)
-		return self.browser.find_element(by, value)
+		random_wait()
+		return wait_for_element(self.browser, by, value)
 	
 	@tabbed
 	def login(self, username, password):
 		browser = self.browser
 		browser.get('https://www.instagram.com/accounts/login/?source=auth_switcher')
-		self.wait()
 		self.wait_for(By.NAME, 'username').send_keys(username)
 		self.wait_for(By.NAME, 'password').send_keys(password)
 		self.wait_for(By.XPATH, '//button[text()="Log in"]').click()
@@ -218,7 +236,7 @@ class Instagram:
 		while browser.current_url != 'https://www.instagram.com/':
 			if browser.current_url == 'https://www.instagram.com/#reactivated':
 				self.wait_for(By.XPATH, '//a[text()="Not Now"]').click()
-			self.wait()
+			random_wait()
 	
 	def logout(self):
 		self.browser.get('https://instagram.com/accounts/logout')
@@ -232,87 +250,20 @@ class Instagram:
 		finally:
 			self.logout()
 	
-	def _search_objects(self, cond, count=-1, context=None):
-		browser = self.browser
-		if context is None:
-			context = browser
-		discovered = set()
-		while True:
-			if 0 <= count <= len(discovered): break
-			self.wait()
-			last_size = len(discovered)
-			try:
-				for post in context.find_elements_by_tag_name('a'):
-					link = post.get_attribute('href')
-					if not cond(link) or link in discovered:
-						continue
-					yield link
-					discovered.add(link)
-					if 0 <= count <= len(discovered): break
-					browser.execute_script("arguments[0].scrollIntoView();", post)
-					break
-				if last_size == len(discovered):
-					break
-			except StaleElementReferenceException:
-				continue
-	
-	def _search_posts(self, count=-1, context=None):
-		for lnk in self._search_objects(is_post_link, count, context):
-			yield Post(get_post_id(lnk))
-	
-	def _search_users(self, count=-1, context=None):
-		for lnk in self._search_objects(is_user_link, count, context):
-			yield User(get_user_id(lnk))
-	
 	@tabbed_multi
 	def posts_by_tag(self, tag, count=-1):
 		browser = self.browser
 		browser.get('https://instagram.com/explore/tags/' + tag)
-		self.wait()
-		return self._search_posts(count)
-	
-	@tabbed_multi
-	def posts_by_user(self, user, count=-1):
-		browser = self.browser
-		browser.get('https://instagram.com/' + user.user_id)
-		self.wait()
-		return self._search_posts(count)
-	
-	@tabbed_multi
-	def posts_by_user(self, user, count=-1):
-		browser = self.browser
-		browser.get('https://instagram.com/' + user.user_id)
-		self.wait()
-		return self._search_posts(count)
+		for lnk in search_objects(self.browser, is_post_link, count):
+			yield get_post_id(lnk)
 	
 	@tabbed_multi
 	def users_recommended(self, count=-1):
 		browser = self.browser
 		browser.get('https://www.instagram.com/explore/people/suggested/')
-		return self._search_users(count)
+		for lnk in search_objects(self.browser, is_user_link, count):
+			yield get_user_id(lnk)
 	
-	@tabbed_multi
-	def users_followers_of(self, user, count=-1):
-		browser = self.browser
-		browser.get('https://www.instagram.com/' + user.user_id)
-		self.wait_for(By.CSS_SELECTOR, 'a[href="/'+user.user_id+'/followers/"]').click()
-		context = self.wait_for(By.CSS_SELECTOR, 'div[role="dialog"]')
-		return self._search_users(count, context)
-	
-	@tabbed_multi
-	def users_followed_by(self, user, count):
-		browser = self.browser
-		browser.get('https://www.instagram.com/' + user.user_id)
-		self.wait_for(By.CSS_SELECTOR, 'a[href="/' + user.user_id + '/following/"]').click()
-		context = self.wait_for(By.CSS_SELECTOR, 'div[role="dialog"]')
-		return self._search_users(count, context)
-
-	def users_who_commented(self, post):
-		pass
-	
-	def users_tagged_in_comment(self, post):
-		pass
-
 
 if __name__ == '__main__':
 	chrome = webdriver.Chrome()
@@ -320,11 +271,8 @@ if __name__ == '__main__':
 	with insta.session(USERNAME, PASSWORD):
 		u = User('naxalpha')
 		with u.open(chrome):
-			for uid in u.followers:
-				print(uid)
-			print('=============================================')
-			for uid in u.followers:
-				print(uid)
+			for pid in u.posts(2):
+				Post(pid).toggle_like()
 	
 	chrome.quit()
 
